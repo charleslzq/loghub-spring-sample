8dbfb4724925b95e3f18d9de08f2f08362a75aaf
Eric Charles <github@ericcharleslzq.com>
2017-10-25 19:20:06 +0800

    add lombok dependency

--- aliyun-loghub-spring-sample/src/main/java/com/github/charleslzq/aliyun/loghub/sample/GroupTopicPrinter.java
+++ aliyun-loghub-spring-sample/src/main/java/com/github/charleslzq/aliyun/loghub/sample/GroupTopicPrinter.java
@@ -2,13 +2,15 @@ package com.github.charleslzq.aliyun.loghub.sample;
 
 import com.aliyun.openservices.log.common.LogGroupData;
 import com.github.charleslzq.aliyun.loghub.listener.filter.LogGroupFilter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class GroupTopicPrinter implements LogGroupFilter {
     @Override
     public boolean accept(LogGroupData logGroupData) {
-        System.out.println(logGroupData.GetLogGroup().getTopic());
+        log.info("Received data from topic {}", logGroupData.GetLogGroup().getTopic());
         return true;
     }
 }
--- aliyun-loghub-spring-sample/src/main/java/com/github/charleslzq/aliyun/loghub/sample/MessageReceiver.java
+++ aliyun-loghub-spring-sample/src/main/java/com/github/charleslzq/aliyun/loghub/sample/MessageReceiver.java
@@ -2,16 +2,18 @@ package com.github.charleslzq.aliyun.loghub.sample;
 
 import com.aliyun.openservices.log.common.Logs;
 import com.github.charleslzq.aliyun.loghub.annotation.LogHubListener;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.stream.Collectors;
 
 @Component
+@Slf4j
 public class MessageReceiver {
 
     @LogHubListener(configName = "consumerConfig", groupFilterBeanNames = "groupTopicPrinter")
-    public void handle(Logs.Log log) {
-        System.out.println(log.getContentsList().stream()
+    public void handle(Logs.Log message) {
+        log.info("Received message {}", message.getContentsList().stream()
                 .collect(Collectors.toMap(
                         Logs.Log.Content::getKey,
                         Logs.Log.Content::getValue
